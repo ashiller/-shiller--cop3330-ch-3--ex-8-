38508f68e5249656c67290cdb409438d415e7902
ashiller <89662655+ashiller@users.noreply.github.com>
2021-11-29 16:43:25 -0500

    Add files via upload

---
+++ practice1cplus/main.cpp
@@ -0,0 +1,25 @@
+/*
+*  UCF COP3330 Fall 2021 Assignment 5 Solution
+*  Copyright 2021 Anthony Shiller
+*/
+#include <iostream>
+#include "std_lib_facilities.h"
+using namespace std;
+
+int main()
+try
+{
+	int val = 0;
+	cout << "Please enter an integer: ";
+	cin >> val;
+	if (!cin) error("something went bad with the read");
+	string res = "even";
+	if (val%2) res = "odd";
+	cout << "The value " << val << " is an " << res << " number\n";
+	keep_window_open();
+}
+catch (runtime_error e) {
+	cout << e.what() << '\n';
+	keep_window_open("~");
+}
+
